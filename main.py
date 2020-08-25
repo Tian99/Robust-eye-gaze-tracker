95b0a49004eeacfd79b3ba3bbc9fe511388156d4
Tian99 <jit29@pitt.edu>
2020-08-24 21:26:31 -0400

    User interface with slight problem

--- main.py
+++ main.py
@@ -3,33 +3,48 @@ import os
 import sys
 import numpy as np
 from eye_tracking.Track import fast_tracker
+from Interface.user import MyWidget
+from PyQt5 import uic, QtCore, QtGui, QtWidgets
 
-class main():
+class main(QtWidgets.QMainWindow):
     def __init__(self, video = None, file = None):
         #Dictionary including index and picture for each
+        super().__init__()
         self.pic_collection = {}
-        self.video = video
-        self.file = file
         self.wanted = None
+        self.MyWidget = None
+
+        uic.loadUi('interface/dum.ui', self)
+        self.setWindowTitle('Pupil Tracking')
+        self.Analyze.setEnabled(False)
+        self.Generate.clicked.connect(self.generate)
+        self.show()
 
     def generate(self):
         #Video for the patient
-        Video = self.video
+        Video = 'input/run1.avi'#self.Video.text()
         #Data retrived by the machine
-        File = self.file
-
+        File = 'instruction.txt'#self.File.text()
         #Check validity
         if not os.path.exists(Video): #or not os.path.exists(File):
-            print('File entered not exist')
+            print('Video entered not exist')
             return
+        if not os.path.exists(File):
+            print("File entered not exist")
+            return
+
         print('Start writing images to the file')
 
         self.wanted = self.to_frame(Video)
-
+        #Just to check for extreme cases, could be ignored for normal cases.
         if self.wanted != None:
             sample = self.pic_collection[self.wanted]
+            #Saved because user.py actually need to read the picture to create the widget
+            cv2.imwrite('input/chosen_pic.png', sample)
+        self.MyWidget = MyWidget(self)
+        self.LayVideo.addWidget(self.MyWidget)
 
-        cv2.imwrite('test.png', sample)
+        self.Analyze.setEnabled(True)
 
     def to_frame(self, video, i = 0):
         maximum = 0
@@ -57,11 +72,12 @@ class main():
 
             self.pic_collection[i] = frame
             i+=1
+            print("image scanned: ", i)
         return wanted
 
 if __name__ == '__main__':
     #Later put into the user interface
-    video = 'input/run1.avi'
-    file = 'Something'
-    Main = main(video, file)
-    Main.generate()
+
+    App = QtWidgets.QApplication([])
+    WINDOW = main()
+    sys.exit(App.exec_())
