12f239a459b826d987ef9ab663db9cef6263e23c
Tian99 <jit29@pitt.edu>
2020-11-26 05:30:41 -0500

    There is a lag between video taking time and file time

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import scipy.stats as stats
+import matplotlib.patches as mpatches
 import numpy as np
 import csv
 
@@ -25,7 +26,13 @@ class auto_draw:
 		self.as_dict = {c[0] : c[1:] for c in self.columns}
 
 	def finish(self, plt, address):
-		plt.legend()
+		red_patch = mpatches.Patch(color='red', label='dly')
+		blue_patch = mpatches.Patch(color='blue', label='cue')
+		green_patch = mpatches.Patch(color='green', label='vgs')
+		c_patch = mpatches.Patch(color='c', label='mgs')
+		black_patch = mpatches.Patch(color='black', label='zscore')
+
+		plt.legend(handles=[red_patch, blue_patch, green_patch, c_patch])
 		plt.grid()
 		plt.savefig(address)
 		plt.close()
@@ -37,35 +44,36 @@ class auto_draw:
 		z_score = stats.zscore(self.as_dict['x'])
 		current = 0
 		mgs_prev = 0
-		plt.plot(self.as_dict['sample'], z_score, 'b-', label = 'zscore')
+		plt.plot(self.as_dict['sample'], z_score, 'black')
+		#Add legend
 		#Specify the colors
 		while(1):
 			self.color = 'b'
 			if self.stare_posi['cue'][current] < limit:
-				plt.plot(self.as_dict['sample'][int(mgs_prev):int(self.stare_posi['cue'][current])], self.as_dict['x'][int(mgs_prev):int(self.stare_posi['cue'][current])], self.color, label = 'cue')
+				plt.plot(self.as_dict['sample'][int(mgs_prev):int(self.stare_posi['cue'][current])], self.as_dict['x'][int(mgs_prev):int(self.stare_posi['cue'][current])], self.color)
 			else:
-				plt.plot(self.as_dict['sample'][int(mgs_prev):int(limit)], self.as_dict['x'][int(mgs_prev):int(limit)], self.color, label = 'cue')
+				plt.plot(self.as_dict['sample'][int(mgs_prev):int(limit)], self.as_dict['x'][int(mgs_prev):int(limit)], self.color)
 				self.finish(plt, address)
 				return
 			self.color = 'g'
 			if self.stare_posi['vgs'][current] < limit:
-				plt.plot(self.as_dict['sample'][int(self.stare_posi['cue'][current]):int(self.stare_posi['vgs'][current])], self.as_dict['x'][int(self.stare_posi['cue'][current]):int(self.stare_posi['vgs'][current])], self.color, label = 'vgs')
+				plt.plot(self.as_dict['sample'][int(self.stare_posi['cue'][current]):int(self.stare_posi['vgs'][current])], self.as_dict['x'][int(self.stare_posi['cue'][current]):int(self.stare_posi['vgs'][current])], self.color)
 			else:
-				plt.plot(self.as_dict['sample'][int(self.stare_posi['cue'][current]):int(limit)], self.as_dict['x'][int(self.stare_posi['cue'][current]):int(limit)], self.color, label = 'vgs')
+				plt.plot(self.as_dict['sample'][int(self.stare_posi['cue'][current]):int(limit)], self.as_dict['x'][int(self.stare_posi['cue'][current]):int(limit)], self.color)
 				self.finish(plt, address)
 				return
 			self.color = 'r'
-			if self.stare_posi['vgs'][current] < limit:
-				plt.plot(self.as_dict['sample'][int(self.stare_posi['vgs'][current]):int(self.stare_posi['dly'][current])], self.as_dict['x'][int(self.stare_posi['vgs'][current]):int(self.stare_posi['dly'][current])], self.color, label = 'dly')
+			if self.stare_posi['dly'][current] < limit:
+				plt.plot(self.as_dict['sample'][int(self.stare_posi['vgs'][current]):int(self.stare_posi['dly'][current])], self.as_dict['x'][int(self.stare_posi['vgs'][current]):int(self.stare_posi['dly'][current])], self.color)
 			else:
-				plt.plot(self.as_dict['sample'][int(self.stare_posi['vgs'][current]):int(limit)], self.as_dict['x'][int(self.stare_posi['vgs'][current]):int(limit)], self.color, label = 'dly')
+				plt.plot(self.as_dict['sample'][int(self.stare_posi['vgs'][current]):int(limit)], self.as_dict['x'][int(self.stare_posi['vgs'][current]):int(limit)], self.color)
 				self.finish(plt, address)
 				return
 			self.color = 'c'
-			if self.stare_posi['vgs'][current] < limit:
-				plt.plot(self.as_dict['sample'][int(self.stare_posi['dly'][current]):int(self.stare_posi['mgs'][current])], self.as_dict['x'][int(self.stare_posi['dly'][current]):int(self.stare_posi['mgs'][current])], self.color, label = 'mgs')
+			if self.stare_posi['mgs'][current] < limit:
+				plt.plot(self.as_dict['sample'][int(self.stare_posi['dly'][current]):int(self.stare_posi['mgs'][current])], self.as_dict['x'][int(self.stare_posi['dly'][current]):int(self.stare_posi['mgs'][current])], self.color)
 			else:
-				plt.plot(self.as_dict['sample'][int(self.stare_posi['dly'][current]):int(limit)], self.as_dict['x'][int(self.stare_posi['dly'][current]):int(limit)], self.color, label = 'mgs')
+				plt.plot(self.as_dict['sample'][int(self.stare_posi['dly'][current]):int(limit)], self.as_dict['x'][int(self.stare_posi['dly'][current]):int(limit)], self.color)
 				self.finish(plt, address)
 				return
 			current += 1
@@ -77,7 +85,7 @@ class auto_draw:
 		plt.ylabel('Y')
 		plt.xlabel('Label')
 		z_score = stats.zscore(self.as_dict['y'])
-		plt.plot(self.as_dict['sample'], z_score, 'b-', label = 'zscore')
+		plt.plot(self.as_dict['sample'], z_score, 'black')
 		plt.plot(self.as_dict['sample'], self.as_dict['y'], self.color, label = 'y')
 
 		self.finish(plt, address)
@@ -86,7 +94,7 @@ class auto_draw:
 		plt.ylabel('R')
 		plt.xlabel('Label')
 		z_score = stats.zscore(self.as_dict['r'])
-		plt.plot(self.as_dict['sample'], z_score, 'b-', label = 'zscore')
+		plt.plot(self.as_dict['sample'], z_score, 'black')
 		plt.plot(self.as_dict['sample'], self.as_dict['r'], self.color, label = 'r')
 
 		self.finish(plt, address)
